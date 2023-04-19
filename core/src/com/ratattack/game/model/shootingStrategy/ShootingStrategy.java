fe29b8b3f83ceaec5607555c6db55c424bccb678
ingval <ingvild.almasbakk@gmail.com>
2023-04-19 10:37:47 +0200

    #60 Textures endres ved oppdatering, venter på riktige textures

--- core/src/com/ratattack/game/model/shootingStrategy/ShootingStrategy.java
+++ core/src/com/ratattack/game/model/shootingStrategy/ShootingStrategy.java
@@ -1,10 +1,14 @@
 package com.ratattack.game.model.shootingStrategy;
 
+import com.badlogic.gdx.graphics.Texture;
+
 import java.util.ArrayList;
 
 public interface ShootingStrategy {
 
-    ShootingStrategy[] strategies = {new NormalBulletStrategy(), new BigBulletStrategy(), new FastBulletStrategy(), new FreezeBulletStrategy(), new TripleBulletStrategy()};
+    ShootingStrategy[] strategies = {new NormalBulletStrategy(), new FastBulletStrategy(), new FreezeBulletStrategy(), new BigBulletStrategy(), new TripleBulletStrategy()};
+    String[] strategyTextures = {"bullet.png", "bullet.png", "freezebullet.png", "BIGbullet.png", "bullet.png"};
+    //String[] strategyTextures = {"normalGrandma.png", "fastGrandma.png", "freezeGrandma.png", "bigGrandma.png", "tripleGrandma.png"};
     int[] prices = {100, 200, 300, 400, 500};
 
     void shoot(int x, int y);
