8d57a439de44056ddca4cf518d408bf42d90d135
ingval <ingvild.almasbakk@gmail.com>
2023-04-20 11:07:32 +0200

    Fixed grandmother and upgradebutton texture updates

--- core/src/com/ratattack/game/model/shootingStrategy/ShootingStrategy.java
+++ core/src/com/ratattack/game/model/shootingStrategy/ShootingStrategy.java
@@ -8,7 +8,7 @@ public interface ShootingStrategy {
 
     ShootingStrategy[] strategies = {new NormalBulletStrategy(), new FastBulletStrategy(), new FreezeBulletStrategy(), new BigBulletStrategy(), new TripleBulletStrategy()};
     //Texture[] strategyTextures = {new Texture("bullet.png"), new Texture("bullet.png"), new Texture("freezebullet.png"), new Texture("BIGbullet.png"), new Texture("bullet.png")};
-    String[] strategyTextures = {"","normalbulletgrandma.png", "fastbulletgrandma.png", "freezebulletgrandma.png", "bigbulletgrandma.png", "triplebulletgrandma.png"};
+    String[] strategyTextures = {"normalbulletgrandma.png", "fastbulletgrandma.png", "freezebulletgrandma.png", "bigbulletgrandma.png", "triplebulletgrandma.png"};
     int[] prices = {100, 200, 300, 400, 500};
 
     void shoot(int x, int y);
