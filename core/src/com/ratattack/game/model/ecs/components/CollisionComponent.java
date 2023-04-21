52687682981fae70bfa31e992311eb902e6fd8bd
rastrand <rastrand@stud.ntnu.no>
2023-04-21 12:05:11 +0200

    Clean up code. Deleted 5 unused files

--- core/src/com/ratattack/game/model/ecs/components/CollisionComponent.java
+++
@@ -1,8 +0,0 @@
-package com.ratattack.game.model.ecs.components;
-
-import com.badlogic.ashley.core.Component;
-
-public class CollisionComponent implements Component {
-
-    //public Family ignoredEntities;
-}
