83db1ce0c32cf59702bc482536a77497fc336fdc
Tuva Djupvik <tuvadjupvik@dhcp-10-220-215-138.wlan.ntnu.no>
2023-04-21 11:35:39 +0200

    Levels

--- core/src/com/ratattack/game/model/shootingStrategy/ShootingStrategy.java
+++ core/src/com/ratattack/game/model/shootingStrategy/ShootingStrategy.java
@@ -9,7 +9,7 @@ public interface ShootingStrategy {
     ShootingStrategy[] strategies = {new NormalBulletStrategy(), new FastBulletStrategy(), new FreezeBulletStrategy(), new BigBulletStrategy(), new TripleBulletStrategy()};
     //Texture[] strategyTextures = {new Texture("bullet.png"), new Texture("bullet.png"), new Texture("freezebullet.png"), new Texture("BIGbullet.png"), new Texture("bullet.png")};
     String[] strategyTextures = {"normalbulletgrandma.png", "fastbulletgrandma.png", "freezebulletgrandma.png", "bigbulletgrandma.png", "triplebulletgrandma.png"};
-    int[] prices = {100, 200, 300, 400, 500};
+    int[] prices = {100, 150, 200, 250, 300};
 
     void shoot(int x, int y);
 }
